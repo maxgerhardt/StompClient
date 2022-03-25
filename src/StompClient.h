3cb453a72c0ba60f1a68091c9ee97bc9386d400b
Maximilian Gerhardt <maximilian.gerhardt@rub.de>
2022-03-25 13:05:40 +0100

    Fix compilation

--- src/StompClient.h
+++ src/StompClient.h
@@ -17,7 +17,7 @@
 
 #include "Stomp.h"
 #include "StompCommandParser.h"
-#include <WebsocketsClient.h>
+#include <WebSocketsClient.h>
 
 namespace Stomp {
 
@@ -60,13 +60,13 @@ class StompClient {
     */
     void begin() {
       // connect to websocket
-      _wsClient.begin(_host, _port, _socketUrl());
+      _wsClient.begin(_host, _port, _socketUrl().c_str());
       _wsClient.setExtraHeaders();
     }
 
     void beginSSL() {
       // connect to websocket
-      _wsClient.beginSSL(_host, _port, _socketUrl());
+      _wsClient.beginSSL(_host, _port, _socketUrl().c_str());
       _wsClient.setExtraHeaders();
     }
 
